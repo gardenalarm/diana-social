d1c0bf062cf996fe9d8baaacc7e9c6c92235964b
rene ae <aehtyb@gmail.com>
2019-09-27 03:59:05 -0500

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+const port = '5000'
+
+const app = new http.Server()
+
+app.on('request', (req, res) => {
+  let method = req.method.toLowerCase()
+  let ip = req.remoteAddress
+  let url = req.url
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.write('ok')
+  res.end('\n')
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
