bf4aff3505ac7352aa74bb0e3b601a1410242bcb
rene ae <aehtyb@gmail.com>
2019-09-29 01:57:09 -0500

    great

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const http = require('http')
 const port = process.env.PORT || 5000
 
 const app = new http.Server()
+var random = Math.random()
+var count = 0
 
 app.on('request', (req, res) => {
+  count++
   let method = req.method.toLowerCase()
   let ip = req.remoteAddress
   let url = req.url
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write(req.method)
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  res.write(`
+    diana.social
+    <hr>
+    Method: ${method}<br>
+    URL: ${req.url}<br>
+    Random: ${Math.random()}<br>
+    Persistent: ${random}<br>
+    Count: ${count}<br>
+    <hr>
+    ${+ new Date()}
+  `)
   res.end('\n')
 });
 
